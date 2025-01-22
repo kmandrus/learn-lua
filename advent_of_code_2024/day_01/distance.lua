bcbbab067492cc116e861ff22f82805ef2a6b4fb
Kevin Andrus <kevin.andrus@flatiron.com>
2025-01-22 17:10:46 -0500

    Naming clarification of day 1, part 1 solution

--- advent_of_code_2024/day_01/distance.lua
+++ advent_of_code_2024/day_01/distance.lua
@@ -47,11 +47,11 @@ local function prepare_list(file, col_num)
 end
 
 
-local function compute_total_distance(first_list, second_list)
+local function compute_total_distance(left_list, right_list)
   local sum = 0
-  for i=1, #first_list do
-    local d = math.abs(first_list[i] - second_list[i])
-    -- print("The distance between " .. first_list[i] .. " and " .. second_list[i] .. " is " .. d)
+  for i=1, #left_list do
+    local d = math.abs(left_list[i] - right_list[i])
+    -- print("The distance between " .. left_list[i] .. " and " .. right_list[i] .. " is " .. d)
     sum = sum + d
   end
   return sum
@@ -59,9 +59,9 @@ end
 
 local function main()
   local puzzle_input = "advent_of_code_2024/day_01/input.txt"
-  local first_list = prepare_list(puzzle_input, 1)
-  local second_list = prepare_list(puzzle_input, 2)
-  local sum = compute_total_distance(first_list, second_list)
+  local left_list = prepare_list(puzzle_input, 1)
+  local right_list = prepare_list(puzzle_input, 2)
+  local sum = compute_total_distance(left_list, right_list)
 
   print("The sum of the distances is " .. sum .. ".")
 end
